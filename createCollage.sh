0728360eba3f93c004da641a97a841b222061748
GasCan1234 <norm@n-franke.de>
2019-11-21 13:33:26 +0100

    change to more templates

--- createCollage.sh
+++ createCollage.sh
@@ -12,22 +12,16 @@ cd $PYTHON_DIR
 mkdir $TMP_DIR
 
 #first convert images
-convert $1 -resize 736x -gravity center -crop 736x410+0+0 $TMP_DIR/output1.jpg
-convert $2 -resize 736x -gravity center -crop 736x410+0+0 $TMP_DIR/output2.jpg
-convert $3 -resize 736x -gravity center -crop 736x410+0+0 $TMP_DIR/output3.jpg
-convert $4 -resize 736x -gravity center -crop 736x410+0+0 $TMP_DIR/output4.jpg
-
-#make collage
-montage -tile 2x2 -geometry +40+10 -background ${BACKGROUND_COLOR} $TMP_DIR/output*.jpg  $TMP_DIR/montage_2x2.png
-
-#crop montage
-convert $TMP_DIR/montage_2x2.png -gravity center -crop 1550x841+0+0 $TMP_DIR/montage_crop.jpg
+convert $1 -resize 725x -gravity center -crop 725x400+0+0 $TMP_DIR/output1.jpg
+convert $2 -resize 725x -gravity center -crop 725x400+0+0 $TMP_DIR/output2.jpg
+convert $3 -resize 725x -gravity center -crop 725x400+0+0 $TMP_DIR/output3.jpg
+convert $4 -resize 725x -gravity center -crop 725x400+0+0 $TMP_DIR/output4.jpg
 
 #set into template
-composite $TMP_DIR/montage_crop.jpg  Template_Maske.png -geometry +100+260 $IMAGE_DIR/${TIME}_Ausdruck.png
+composite $TMP_DIR/output1.jpg  print_templates/epages/Template_Maske_1.jpg -geometry +134+164 $IMAGE_DIR/${TIME}_Ausdruck.png
 
 #print Ausdruck
-lp -d photo_printer -n ${COUNT_PRINTS} $IMAGE_DIR/${TIME}_Ausdruck.png -o landscape -o fit-to-page -o media=4x6
+#lp -d photo_printer -n ${COUNT_PRINTS} $IMAGE_DIR/${TIME}_Ausdruck.png -o landscape -o fit-to-page -o media=4x6
 
 #remove tmp_dir
 rm -rf $TMP_DIR
